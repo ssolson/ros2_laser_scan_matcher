554012129469573041ab0f510aa49337c81d968d
ssolson <sterlingolson@yahoo.com>
2024-12-16 16:49:32 -0500

    fix use sim time

--- launch/start_matcher.launch.py
+++ launch/start_matcher.launch.py
@@ -1,30 +1,31 @@
-import os
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-
-
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     odometry_node = Node(
         package='ros2_laser_scan_matcher',
+        executable='laser_scan_matcher',
+        name='odometry_publisher',
         parameters=[{
             'base_frame': 'base_link',
             'odom_frame': 'odom_matcher',
             'laser_frame': 'laser_frame',
             'publish_odom': '/odom_matcher',
-            'publish_tf': True
-        }],
-        executable='laser_scan_matcher',
-        name='odometry_publisher',
+            'publish_tf': True,
+            'use_sim_time': use_sim_time  # Pass use_sim_time here
+        }]
     )
 
     # Create and return the Launch description
     return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                             description='Flag to enable use_sim_time'),
+        launch.actions.DeclareLaunchArgument(
+            name='use_sim_time',
+            default_value='True',
+            description='Flag to enable use_sim_time'
+        ),
         odometry_node
     ])
